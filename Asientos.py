daa8a15658299a4c6e2f3838999b4b48aa09162f
Jossian <A01746745@itesm.mx>
2018-02-01 22:04:07 -0600

    Problema asientos completado

--- Asientos.py
+++ Asientos.py
@@ -1,1 +1,18 @@
 #Autor: Jossian Abimelec García Quijano
+#calcula el valor a pagar a partir de los asientos
+
+
+def calcularPago(numeroBoletosA, numeroBoletosB, numeroBoletosC):
+    pago=numeroBoletosA*870+numeroBoletosB*650+numeroBoletosC*235
+    return pago
+
+
+
+def main():
+    numeroBoletosA = int(input("Teclea el número de asientos clase A: "))
+    numeroBoletosB = int(input("Teclea el número de asientos clase B: "))
+    numeroBoletosC = int(input("Teclea el número de asientos clase C: "))
+    pago=calcularPago(numeroBoletosA,numeroBoletosB,numeroBoletosC)
+    print("el costo total es: $%.2f "%pago)
+
+main()
