0c425084ab5190c149dece1a01e7bb48be349efa
Jossian <A01746745@itesm.mx>
2018-02-02 14:34:59 -0600

    Problema pago completado

---
+++ pagoTrabajador.py
@@ -0,0 +1,24 @@
+#Jossian Abimelec García Quijano
+#Calcula
+
+
+def calcularPago(horasnormales, pagohora):
+    pago = horasnormales*pagohora
+    return pago
+
+
+def calcularPagoextra(horasextras, pagohora):
+    pagoextra = (horasextras*pagohora)*1.75
+    return pagoextra
+
+
+def main():
+    horasnormales = int(input("Teclea en número de horas normales trabajadas: "))
+    horasextras = int(input("Teclea en número de horas extras trabajadas: "))
+    pagohora = int(input("Teclea el pago por hora: "))
+    pago = calcularPago(horasnormales, pagohora)
+    pagoextra = calcularPagoextra(horasextras, pagohora)
+    print("Pago normal: $%.2f"%pago)
+    print("Pago extra: $%.2f" % pagoextra)
+    print("Pago total: $%.2f"%(pago+pagoextra))
+main()
